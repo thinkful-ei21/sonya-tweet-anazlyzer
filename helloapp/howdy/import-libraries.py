9882c20b087585e79f3b94c2441ae3f4d19be3f7
Sonya <sonjay08@gmail.com>
2018-08-16 16:20:34 -0500

    added global username

--- helloapp/howdy/import-libraries.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-files = ["negative.txt", "positive.txt"]
-path = "http://www.unc.edu/~ncaren/haphazard/"
-for file_name in files:
-    urllib.request.urlretrieve(path+file_name, file_name)
