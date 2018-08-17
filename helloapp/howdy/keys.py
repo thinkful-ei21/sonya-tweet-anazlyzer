5106f8fa2f6cda670633dcfb6da68fa4f8b4ffe6
Sonya <sonjay08@gmail.com>
2018-08-17 11:42:16 -0500

    twitter anazlyzer functioning

--- helloapp/howdy/keys.py
+++ helloapp/howdy/keys.py
@@ -1,2 +1,2 @@
-consumer_key = "YiyW8ZmfLzL1QFGkJM1bZuUUv"
-consumer_secret = "lsQOg4kaQdYkqoscK7LPqaCpiHWWVY4b0RcQnESgMoPAoZlm8j"
+client_key = "YiyW8ZmfLzL1QFGkJM1bZuUUv"
+client_secret = "lsQOg4kaQdYkqoscK7LPqaCpiHWWVY4b0RcQnESgMoPAoZlm8j"
--- helloapp/howdy/text-analyzer.py
+++ helloapp/howdy/text-analyzer.py
@@ -1,11 +1,11 @@
 from textblob import TextBlob
 import base64
+import pprint
 import requests
-import consumer_key from "./keys.py"
-import consumer_secret from "./keys.py"
-# consumer_key = "YiyW8ZmfLzL1QFGkJM1bZuUUv"
-# consumer_secret = "lsQOg4kaQdYkqoscK7LPqaCpiHWWVY4b0RcQnESgMoPAoZlm8j"
-key_secret = "{}:{}".format(consumer_key, consumer_secret).encode("ascii")
+from keys import client_key
+from keys import client_secret
+
+key_secret = "{}:{}".format(client_key, client_secret).encode("ascii")
 b64_encoded_key = base64.b64encode(key_secret)
 b64_encoded_key = b64_encoded_key.decode("ascii")
 
@@ -29,53 +29,60 @@ search_headers = {
   "Authorization": "Bearer {}".format(access_token)
 }
 
-search_params = {
-  "q": "trump middle east",
-  "result_type": "recent",
-  "lang": "en",
-  "count": 3
-}
+tweet_data = []
 
-search_url = "{}1.1/search/tweets.json".format(base_url)
 
-search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+def get_tweet_ids_and_text(data):
+    tweet_dict = {}
+    for x in data:
+        tweet_dict[x["id"]] = x["text"]
+    return tweet_dict
 
-tweet_data = search_resp.json()
-import pprint
-pprint.pprint(tweet_data)
 
-tweets = {}
+def get_tweets():
+    tweet_counter = 0
+    max_id = None
+    while tweet_counter < 1000:
+        search_url = "{}1.1/search/tweets.json".format(base_url)
+        search_params = {
+            "q": "Trump hate bastard fucking",
+            "result_type": "recent",
+            "lang": "en",
+            "count": 100,
+            "max_id": max_id
+        }
+        search_resp = requests.get(search_url, headers=search_headers, params=search_params)    
+        tweets_json = search_resp.json()
+        tweet_info = tweets_json["statuses"]
+        for x in tweet_info:
+            tweet_data.append(x)
+        # get lowest id from tweet data
+        tweet_ids = []
+        for x in tweets_json["statuses"]:
+            tweet_ids.append(x["id"])
+            lowest_id = tweet_ids[0]
+            for id in tweet_ids:
+                if id < lowest_id:
+                    lowest_id = id
+        max_id = lowest_id - 1
+        tweet_counter += 100
 
+
+get_tweets()
 # pull out text and id from tweet data
-for x in tweet_data["statuses"]:
-    tweets[x["id"]] = x["text"]
-
-unique_tweets = {}
+processed_tweet_data = get_tweet_ids_and_text(tweet_data)
 
 # remove all duplicate tweets
-for key, value in tweets.items():
-    if value not in unique_tweets.values():
-        unique_tweets[key] = value
-
-# print(unique_tweets)
-# print(len(unique_tweets), len(tweets))
-
-tweet_texts = ""
+unique_tweets = {}
+for tweet in processed_tweet_data:
+        if tweet not in unique_tweets:
+            unique_tweets[tweet] = processed_tweet_data[tweet]
 
 # create a list of tweet text
+tweet_texts = ""
 for key in unique_tweets.values():
     tweet_texts += key
 
-# print(tweet_texts)
+text = TextBlob(" ".join(tweet_texts))
 
-
-# text = TextBlob(" ".join(tweet_texts))
-# text = TextBlob('''
-# I do not like this car.
-# This view is horrible.
-# I feel tired this morning.
-# I am not looking forward to the concert.
-# He is my enemy.
-# ''')
-
-# print(text.sentiment)
+print(text.sentiment)
